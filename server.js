8bd19c526985456520bc3a7d00011961b8057a78
Hugo Batista <hugojvb20@gmail.com>
2020-09-28 20:08:24 +0100

    initial server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Server running at port: ", PORT));
