2e30847958a9e73535f15eefe389a46c03903d01
Hugo <hugojvb20@gmail.com>
2020-10-01 16:51:57 +0100

    pushing only non ignore files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const users = require("./routes/users.js");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+connectDB();
+const app = express();
+
+// use body parser
+app.use(express.json());
+
+app.use("/users", users);
+
+// listenning at port 5000
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () =>
+  console.log("Server running at port: ", PORT)
+);
+
+// error handling
+process.on("unhandledRejection", (err, promise) => {
+  console.log("Error: ", err.message);
+  server.close(() => process.exit(1));
+});
