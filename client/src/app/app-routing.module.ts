e66902354708b5525c7a4ec165dd25f6c72949f8
Hugo <hugojvb20@gmail.com>
2020-10-12 21:25:34 +0100

    Finished signin component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { BodyComponent } from './components/body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  {
+    path: 'signin',
+    component: SigninComponent,
+    children: [{ path: '', component: SigninComponent }],
+  },
+  { path: 'transactions', component: BodyComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgForOf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  signedIn: boolean = false;
 }
