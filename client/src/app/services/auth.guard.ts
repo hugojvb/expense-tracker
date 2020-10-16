09940e78909df36b29b58cfa37ba1e1641fe3a75
Hugo Batista <hugojvb20@gmail.com>
2020-10-16 19:16:35 +0100

    Redirect to /signin in protected routes

--- client/src/app/services/auth.guard.ts
+++ client/src/app/services/auth.guard.ts
@@ -2,23 +2,30 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
 } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 import { SignInService } from './signin.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private signInService: SignInService) {}
+  constructor(private signInService: SignInService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
   ): boolean | Promise<boolean> | Observable<boolean> {
     return this.signInService.userToken.pipe(
+      take(1),
       map((userToken) => {
         return !!userToken;
+      }),
+      tap((isSignedIn) => {
+        if (!isSignedIn) {
+          this.router.navigate(['/signin']);
+        }
       })
     );
   }
