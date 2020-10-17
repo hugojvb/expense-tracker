12c4c8865abecafe06a8449dbc0c064cfc7df962
Hugo Batista <hugojvb20@gmail.com>
2020-10-17 23:40:18 +0100

    Added goals and history components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { SigninComponent } from './components/signin/signin.component';
 import { BodyComponent } from './components/body/body.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AuthGuard } from './services/auth.guard';
+import { GoalsComponent } from './components/goals/goals.component';
+import { HistoryComponent } from './components/history/history.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/transactions', pathMatch: 'full' },
@@ -13,8 +15,9 @@ const routes: Routes = [
     children: [{ path: '', component: SigninComponent }],
   },
   { path: 'transactions', component: BodyComponent, canActivate: [AuthGuard] },
-
   { path: 'signup', component: SignupComponent },
+  { path: 'goals', component: GoalsComponent, canActivate: [AuthGuard] },
+  { path: 'history', component: HistoryComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
