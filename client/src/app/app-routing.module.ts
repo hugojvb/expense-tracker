9dbac9e7d905ead333b51322cdb6e66bad96220d
Hugo Batista <hugojvb20@gmail.com>
2020-10-17 13:30:20 +0100

    Changed / to /transactions

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: '', redirectTo: '/transactions', pathMatch: 'full' },
   {
     path: 'signin',
     component: SigninComponent,
