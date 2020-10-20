248c3f2c2d88ebcfb8e41f28c1bd06ae7d8aed9c
Hugo Batista <hugojvb20@gmail.com>
2020-10-20 12:03:58 +0100

    Added history tab with loading spinner

--- client/src/app/components/history/history.component.ts
+++ client/src/app/components/history/history.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  styleUrls: ['./history.component.css'],
 })
 export class HistoryComponent implements OnInit {
-
-  constructor() { }
+  constructor(private DataService: DataService) {}
+  data;
+  loading: boolean = true;
 
   ngOnInit(): void {
+    this.DataService.getTransactions().subscribe((res) => {
+      this.data = res.data;
+      this.loading = false;
+    });
   }
-
 }
