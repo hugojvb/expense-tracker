9dbc0fea8fe02143e761628d334c59b334169f06
Hugo Batista <hugojvb20@gmail.com>
2020-10-23 15:42:51 +0100

    Changed limit for ngFor instead of the backend

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,7 +3,7 @@ const TransactionsSchema = require("../models/TransactionsSchema");
 // get all transactions
 exports.getTransactions = async (req, res) => {
   try {
-    const transactions = await TransactionsSchema.find({ user: req.user }).limit(15);
+    const transactions = await TransactionsSchema.find({ user: req.user });
 
     res.status(200).json({ success: true, data: transactions, count: transactions.length });
   } catch (error) {
