c27e4aa993932f3b7b5eb0ed185606106b4db560
Hugo Batista <hugojvb20@gmail.com>
2021-02-20 12:06:43 +0000

    Added context to app

---
+++ client/src/context/state.tsx
@@ -0,0 +1,36 @@
+import React, { useReducer } from "react";
+import Context from "./context";
+import reducer from "./reducer";
+import { LOGIN, LOGOUT } from "./types";
+
+const State = (props: any) => {
+	const initialState = {
+		logged_in: false,
+	};
+
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	// actions
+	const login = () => {
+		dispatch({ type: LOGIN });
+	};
+
+	const logout = () => {
+		dispatch({ type: LOGOUT });
+	};
+
+	return (
+		<Context.Provider
+			value={{
+				logged_in: state.logged_in,
+
+				login,
+				logout,
+			}}
+		>
+			{props?.children}
+		</Context.Provider>
+	);
+};
+
+export default State;
