81056ec6cc51032c7c231a5475de970cf5e5dd01
Hugo Batista <hugojvb20@gmail.com>
2021-08-21 20:57:18 +0100

    Added sample drawer

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,18 +2,18 @@ import { FC, Fragment, useState, useContext, MouseEvent } from "react";
 
 // MATERIAL-UI COMPONENTS IMPORT
 import {
-	makeStyles,
-	Theme,
-	createStyles,
-	AppBar,
-	Typography,
-	Toolbar,
-	IconButton,
-	Menu,
-	MenuItem,
-	withStyles,
-	ListItemIcon,
-	ListItemText,
+  makeStyles,
+  Theme,
+  createStyles,
+  AppBar,
+  Typography,
+  Toolbar,
+  IconButton,
+  Menu,
+  MenuItem,
+  withStyles,
+  ListItemIcon,
+  ListItemText,
 } from "@material-ui/core";
 
 // MATERIAL-UI ICONS IMPORT
@@ -31,143 +31,153 @@ import Context from "../context/context";
 
 // MAKE STYLES
 const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {
-			flexGrow: 1,
-		},
-		brand: {
-			flexGrow: 1,
-			marginLeft: 20,
-			cursor: "pointer",
-		},
-		menu: {
-			marginTop: "5vh",
-		},
-		link: {
-			textDecoration: "none",
-			color: "#444",
-		},
-	})
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    brand: {
+      flexGrow: 1,
+      marginLeft: 20,
+      cursor: "pointer",
+    },
+    menu: {
+      marginTop: "5vh",
+    },
+    link: {
+      textDecoration: "none",
+      color: "#444",
+    },
+  })
 );
 
 // CUSTOM MENU ITEM COMPONENT
 const StyledMenuItem = withStyles((theme) => ({
-	root: {
-		"&:hover": {
-			backgroundColor: theme.palette.primary.main,
-			"& .MuiListItemIcon-root, & .MuiListItemText-primary": {
-				color: theme.palette.common.white,
-			},
-		},
-	},
+  root: {
+    "&:hover": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white,
+      },
+    },
+  },
 }))(MenuItem);
 
 // FUNCTIONAL COMPONENT
 const Navbar: FC = (): JSX.Element => {
-	// USE STYLES HOOK
-	const classes = useStyles();
+  // USE STYLES HOOK
+  const classes = useStyles();
 
-	// USE CONTEXT HOOK
-	const context = useContext(Context);
+  // USE CONTEXT HOOK
+  const context = useContext(Context);
 
-	// MENU STATE
-	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-	const openMenu = Boolean(anchorEl);
+  // MENU STATE
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const openMenu = Boolean(anchorEl);
 
-	// OPEN MENU
-	const handleMenu = (event: MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
+  // OPEN MENU
+  const handleMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	// CLOSE MENU
-	const handleCloseMenu = () => {
-		setAnchorEl(null);
-	};
+  // CLOSE MENU
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
 
-	// LOGOUT STATE
-	const { logout } = context;
+  // LOGOUT STATE
+  const { logout } = context;
 
-	// CHANGE LOGGEDIN STATE
-	const onLogout = () => {
-		if (logout) {
-			logout();
-		}
-	};
+  // CHANGE LOGGEDIN STATE
+  const onLogout = () => {
+    if (logout) {
+      logout();
+    }
+  };
 
-	return (
-		<Fragment>
-			<AppBar position="sticky" className={classes.root} color="primary">
-				<Toolbar>
-					<img src="/logo.png" alt="logo" height="40" width="50" onClick={() => (window.location.pathname = "/")} />
-					<Typography variant="h6" className={classes.brand} onClick={() => (window.location.pathname = "/")}>
-						Expense Tracker
-					</Typography>
+  return (
+    <Fragment>
+      <AppBar position="sticky" className={classes.root} color="primary">
+        <Toolbar>
+          <img
+            src="/logo.png"
+            alt="logo"
+            height="40"
+            width="50"
+            onClick={() => (window.location.pathname = "/")}
+          />
+          <Typography
+            variant="h6"
+            className={classes.brand}
+            onClick={() => (window.location.pathname = "/")}
+          >
+            Expense Tracker
+          </Typography>
 
-					<div>
-						<IconButton
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleMenu}
-							color="inherit"
-						>
-							<AccountCircle />
-						</IconButton>
-						<Menu
-							elevation={4}
-							id="menu-appbar"
-							anchorEl={anchorEl}
-							anchorOrigin={{
-								vertical: "bottom",
-								horizontal: "center",
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: "top",
-								horizontal: "center",
-							}}
-							open={openMenu}
-							onClose={handleCloseMenu}
-							className={classes.menu}
-						>
-							<Link to="/home" className={classes.link}>
-								<StyledMenuItem onClick={handleCloseMenu}>
-									<ListItemIcon>
-										<HomeIcon fontSize="small" />
-									</ListItemIcon>
-									<ListItemText primary="Home" />
-								</StyledMenuItem>
-							</Link>
-							<Link to="/goals" className={classes.link}>
-								<StyledMenuItem onClick={handleCloseMenu}>
-									<ListItemIcon>
-										<TrackChangesIcon fontSize="small" />
-									</ListItemIcon>
-									<ListItemText primary="Goals" />
-								</StyledMenuItem>
-							</Link>
-							<Link to="/history" className={classes.link}>
-								<StyledMenuItem onClick={handleCloseMenu}>
-									<ListItemIcon>
-										<HistoryIcon fontSize="small" />
-									</ListItemIcon>
-									<ListItemText primary="History" />
-								</StyledMenuItem>
-							</Link>
-							<Link to="/login" className={classes.link} onClick={onLogout}>
-								<StyledMenuItem onClick={handleCloseMenu}>
-									<ListItemIcon>
-										<ExitToAppIcon fontSize="small" />
-									</ListItemIcon>
-									<ListItemText primary="Logout" />
-								</StyledMenuItem>
-							</Link>
-						</Menu>
-					</div>
-				</Toolbar>
-			</AppBar>
-		</Fragment>
-	);
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              elevation={4}
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+              open={openMenu}
+              onClose={handleCloseMenu}
+              className={classes.menu}
+            >
+              <Link to="/home" className={classes.link}>
+                <StyledMenuItem onClick={handleCloseMenu}>
+                  <ListItemIcon>
+                    <HomeIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Home" />
+                </StyledMenuItem>
+              </Link>
+              <Link to="/goals" className={classes.link}>
+                <StyledMenuItem onClick={handleCloseMenu}>
+                  <ListItemIcon>
+                    <TrackChangesIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Goals" />
+                </StyledMenuItem>
+              </Link>
+              <Link to="/history" className={classes.link}>
+                <StyledMenuItem onClick={handleCloseMenu}>
+                  <ListItemIcon>
+                    <HistoryIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="History" />
+                </StyledMenuItem>
+              </Link>
+              <Link to="/login" className={classes.link} onClick={onLogout}>
+                <StyledMenuItem onClick={handleCloseMenu}>
+                  <ListItemIcon>
+                    <ExitToAppIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText primary="Logout" />
+                </StyledMenuItem>
+              </Link>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
+  );
 };
 
 export default Navbar;
