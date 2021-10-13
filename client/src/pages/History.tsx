99a454ee156ca0051180fb5e2a05751e871de8be
hugojvb <hugojvb20@gmail.com>
2021-10-13 10:09:01 +0100

    Changed history component

--- client/src/pages/History.tsx
+++ client/src/pages/History.tsx
@@ -1,61 +1,17 @@
-import { Fragment, useContext } from "react";
+import { Fragment } from "react";
 
-// COMPONENTS
 import HistoryTable from "../components/HistoryTable/HistoryTable";
-import Navbar from "../components/Navbar";
 
-// MATERIAL-UI COMPONENTS
-import { Box, makeStyles, createStyles } from "@material-ui/core";
-import clsx from "clsx";
+import { makeStyles, createStyles } from "@material-ui/core";
 
-// CONTEXT API
-import Context from "../context/context";
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) =>
-	createStyles({
-		drawerHeader: {
-			display: "flex",
-			alignItems: "center",
-			padding: theme.spacing(0, 1),
-			// necessary for content to be below app bar
-			...theme.mixins.toolbar,
-			justifyContent: "flex-start",
-		},
-		content: {
-			flexGrow: 1,
-			padding: theme.spacing(7, 6),
-			transition: theme.transitions.create("margin", {
-				easing: theme.transitions.easing.sharp,
-				duration: theme.transitions.duration.leavingScreen,
-			}),
-			marginLeft: drawerWidth,
-		},
-		contentShift: {
-			transition: theme.transitions.create("margin", {
-				easing: theme.transitions.easing.easeOut,
-				duration: theme.transitions.duration.enteringScreen,
-			}),
-			marginLeft: theme.spacing(7.5) + 1,
-		},
-	})
-);
+const useStyles = makeStyles((theme) => createStyles({}));
 
 export default function DemoInstances() {
 	const classes = useStyles();
 
-	const context = useContext(Context);
-
 	return (
 		<Fragment>
-			<Box
-				className={clsx(classes.content, {
-					// [classes.contentShift]: !drawer,
-				})}
-			>
-				<HistoryTable />
-			</Box>
+			<HistoryTable />
 		</Fragment>
 	);
 }
