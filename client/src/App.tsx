931d91f74a685bd5d0e04ff58c52817bf40727dd
hugojvb <hugojvb20@gmail.com>
2021-10-18 22:05:38 +0100

    Added react cookie

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect } from "react";
 
 import Context from "./context/context";
 
@@ -10,32 +10,43 @@ import Login from "./pages/Login";
 import Layout from "./components/Layout";
 
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { CookiesProvider } from "react-cookie";
 
 // FUNCTIONAL COMPONENT
 const App = (): JSX.Element => {
 	const context = useContext(Context);
 
 	// LOGGED IN STATE
-	const { loggedIn } = context;
+	const { loggedIn, login } = context;
+
+	const [cookies, setCookie] = useCookies();
+
+	useEffect(() => {
+		if (cookies.token) login();
+		console.log(cookies.token);
+	}, []);
 
 	return (
 		<Router>
-			<Layout>
-				<Route exact path="/">
-					{!loggedIn && <Redirect to="/login" />}
-					<Home />
-				</Route>
-				<Route exact path="/history" component={History}>
-					{!loggedIn && <Redirect to="/login" />}
-				</Route>
-				<Route exact path="/goals" component={Goals}>
-					{!loggedIn && <Redirect to="/login" />}
-				</Route>
-				<Route exact path="/login">
-					{loggedIn && <Redirect to="/" />}
-					<Login />
-				</Route>
-			</Layout>
+			<CookiesProvider>
+				<Layout>
+					<Route exact path="/">
+						{!loggedIn && <Redirect to="/login" />}
+						<Home />
+					</Route>
+					<Route exact path="/history" component={History}>
+						{!loggedIn && <Redirect to="/login" />}
+					</Route>
+					<Route exact path="/goals" component={Goals}>
+						{!loggedIn && <Redirect to="/login" />}
+					</Route>
+					<Route exact path="/login">
+						{loggedIn && <Redirect to="/" />}
+						<Login />
+					</Route>
+				</Layout>
+			</CookiesProvider>
 		</Router>
 	);
 };
