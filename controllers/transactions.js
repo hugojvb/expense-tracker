2706d1f5220cd2458bbbf5203580ab3dd3c4f3ac
hugojvb <hugojvb20@gmail.com>
2021-10-21 19:24:55 +0100

    Fixed getTransactions order

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -2,62 +2,78 @@ const TransactionsSchema = require("../models/TransactionsSchema");
 
 // get all transactions
 exports.getTransactions = async (req, res) => {
-  try {
-    const transactions = await TransactionsSchema.find({ user: req.user });
+	try {
+		const transactions = await TransactionsSchema.find({
+			user: req.user,
+		}).sort({ date: -1 });
 
-    res.status(200).json({ success: true, data: transactions, count: transactions.length });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ Error: "Failed getting transactions" });
-  }
+		res.status(200).json({
+			success: true,
+			data: transactions,
+			count: transactions.length,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ Error: "Failed getting transactions" });
+	}
 };
 
 // add transaction
 exports.addTransaction = async (req, res, next) => {
-  try {
-    req.body.date = new Date();
-    req.body.user = req.user;
-    const newTransaction = await TransactionsSchema.create(req.body);
+	try {
+		req.body.date = new Date();
+		req.body.user = req.user;
+		const newTransaction = await TransactionsSchema.create(req.body);
 
-    res.status(201).json({ success: true, data: newTransaction });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ Error: "Failed to add a new transaction" });
-  }
+		res.status(201).json({ success: true, data: newTransaction });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ Error: "Failed to add a new transaction" });
+	}
 };
 
 // update transaction
 exports.updateTransaction = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const transaction = await TransactionsSchema.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+	try {
+		const { id } = req.params;
+		const transaction = await TransactionsSchema.findByIdAndUpdate(
+			id,
+			req.body,
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
 
-    if (!transaction) return res.status(400).json({ Error: "Id not found or Input incorrect" });
+		if (!transaction)
+			return res
+				.status(400)
+				.json({ Error: "Id not found or Input incorrect" });
 
-    res.status(200).json({
-      success: true,
-      data: transaction,
-    });
-  } catch (error) {
-    res.status(400).json({ Error: "Failed to update transaction" });
-  }
+		res.status(200).json({
+			success: true,
+			data: transaction,
+		});
+	} catch (error) {
+		res.status(400).json({ Error: "Failed to update transaction" });
+	}
 };
 
 // delete transaction
 exports.deleteTransaction = async (req, res) => {
-  try {
-    const transaction = await TransactionsSchema.findByIdAndDelete(req.params.id);
-    if (!transaction) return res.status(400).json({ Error: "Id not found" });
+	try {
+		const transaction = await TransactionsSchema.findByIdAndDelete(
+			req.params.id
+		);
+		if (!transaction)
+			return res.status(400).json({ Error: "Id not found" });
 
-    res.status(200).json({
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ Error: "Failed to delete transaction" });
-  }
+		res.status(200).json({
+			success: true,
+			data: {},
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ Error: "Failed to delete transaction" });
+	}
 };
