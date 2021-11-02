cc583250db0eaada33c16e65cb6dcb1f96199175
hugojvb <hugojvb20@gmail.com>
2021-11-02 23:50:09 +0000

    Changed get goals controller

--- controllers/goals.js
+++ controllers/goals.js
@@ -1,40 +1,46 @@
 const GoalsSchema = require("../models/GoalsSchema");
 
 exports.getGoals = async (req, res) => {
-  try {
-    const goals = await GoalsSchema.find({ user: req.user });
+	try {
+		const goals = await GoalsSchema.find({ user: req.user }).sort({
+			date: -1,
+		});
 
-    res.status(200).json({ success: true, data: goals, count: goals.length });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ Error: "Failed getting goals" });
-  }
+		res.status(200).json({
+			success: true,
+			data: goals,
+			count: goals.length,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ Error: "Failed getting goals" });
+	}
 };
 
 exports.setGoals = async (req, res) => {
-  try {
-    req.body.date = new Date();
-    req.body.user = req.user;
-    const newGoal = await GoalsSchema.create(req.body);
+	try {
+		req.body.date = new Date();
+		req.body.user = req.user;
+		const newGoal = await GoalsSchema.create(req.body);
 
-    res.status(201).json({ success: true, data: newGoal });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ Error: "Failed to add a new goal" });
-  }
+		res.status(201).json({ success: true, data: newGoal });
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ Error: "Failed to add a new goal" });
+	}
 };
 
 exports.deleteGoals = async (req, res) => {
-  try {
-    const goal = await GoalsSchema.findByIdAndDelete(req.params.id);
-    if (!goal) return res.status(400).json({ Error: "Id not found" });
+	try {
+		const goal = await GoalsSchema.findByIdAndDelete(req.params.id);
+		if (!goal) return res.status(400).json({ Error: "Id not found" });
 
-    res.status(200).json({
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ Error: "Failed to delete goal" });
-  }
+		res.status(200).json({
+			success: true,
+			data: {},
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({ Error: "Failed to delete goal" });
+	}
 };
