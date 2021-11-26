88211387f7eab73d1477bbbbf2b16e4cb8e14b10
hugojvb <hugojvb20@gmail.com>
2021-11-26 19:26:38 +0000

    Changed stats routing to get stats

--- routes/stats.js
+++ routes/stats.js
@@ -12,18 +12,6 @@ const router = express.Router();
 
 const { verify } = require("../middleware/verify.js");
 
-router.get("/lastmonth", verify, getLastMonth);
-
-router.get("/lastsemestermean", verify, getLastSemesterMean);
-
-router.get("/lastgoal", verify, getLastGoal);
-
-router.get("/spentthismonth", verify, getSpentThisMonth);
-
-router.get("/highestspentmonth", verify, getHighestSpentMonth);
-
-router.get("/lowestspentmonth", verify, getLowestSpentMonth);
-
-router.get("/last12months", verify, getLast12MonthsExpenses);
+router.get("/stats", verify, getStats);
 
 module.exports = router;
