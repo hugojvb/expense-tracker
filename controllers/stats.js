3f1dc2056f34a257ef64a758150a2cb5cc8e94ee
hugojvb <hugojvb20@gmail.com>
2021-11-26 19:35:48 +0000

    Changed stats controller to have one function

--- controllers/stats.js
+++ controllers/stats.js
@@ -9,8 +9,10 @@ const {
 	getLast12MonthsExpensesService,
 } = require("../services/stats");
 
-exports.getLastMonth = async (req, res) => {
+exports.getStats = async (req, res) => {
 	try {
+		let stats;
+
 		const lastMonth = await TransactionsSchema.find({
 			user: req.user,
 			date: {
@@ -22,83 +24,45 @@ exports.getLastMonth = async (req, res) => {
 		const totalAmount = Math.round(lastMonth.reduce((sum, current) => (sum += current.amount), 0));
 		if (!lastMonth) return res.status(400).json({ Error: "Last month transactions not found" });
 
-		res.status(200).json({ success: true, data: totalAmount });
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ Error: "Failed to get last month" });
-	}
-};
+		stats.totalAmount = totalAmount;
 
-exports.getLastSemesterMean = async (req, res) => {
-	try {
 		const semesterMean = await getLastSemesterMeanService(req.user);
 
 		if (!semesterMean) return res.status(400).json({ Error: "Last semester transactions mean not found" });
 
-		res.status(200).json({ success: true, data: semesterMean });
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ Error: "Failed to get last semester mean" });
-	}
-};
+		stats.semesterMean = semesterMean;
 
-exports.getLastGoal = async (req, res) => {
-	try {
 		const lastGoal = await getLastGoalService(req.user);
 
 		if (!lastGoal) return res.status(400).json({ Error: "Last goal not found" });
 
-		res.status(200).json({ success: true, data: lastGoal.goal });
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ Error: "Failed to get last goal" });
-	}
-};
+		stats.lastGoal = lastGoal.goal;
 
-exports.getSpentThisMonth = async (req, res) => {
-	try {
 		const spentThisMonth = await getSpentThisMonthService(req.user);
 
 		if (!spentThisMonth) return res.status(400).json({ Error: "Spent this month not found" });
 
-		res.status(200).json({ success: true, data: spentThisMonth });
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ Error: "Failed to get spent this month" });
-	}
-};
+		stats.spentThisMonth = spentThisMonth;
 
-exports.getHighestSpentMonth = async (req, res) => {
-	try {
 		const { max, maxMonth } = await getHighestAndLowestSpentMonthService(req.user);
 
 		if (!max) return res.status(400).json({ Error: "Failed to get highest spent month" });
 
-		return res.status(200).json({ data: { max, maxMonth } });
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ Error: error });
-	}
-};
+		stats.max = max;
+		stats.maxMonth = maxMonth;
 
-exports.getLowestSpentMonth = async (req, res) => {
-	try {
 		const { min, minMonth } = await getHighestAndLowestSpentMonthService(req.user);
 
 		if (!min) return res.status(400).json({ Error: "Failed to get lowest spent month" });
 
-		return res.status(200).json({ data: { min, minMonth } });
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ Error: error });
-	}
-};
+		stats.min = min;
+		stats.minMonth = minMonth;
 
-exports.getLast12MonthsExpenses = async (req, res) => {
-	try {
 		const monthExpensesArray = await getLast12MonthsExpensesService(req.user);
 
-		return res.status(200).json({ data: monthExpensesArray });
+		stats.monthExpensesArray = monthExpensesArray;
+
+		return res.status(200).json({ data: stats });
 	} catch (error) {
 		console.log(error);
 		res.status(500).json({ Error: error });
--- routes/stats.js
+++ routes/stats.js
@@ -1,13 +1,5 @@
 const express = require("express");
-const {
-	getLastMonth,
-	getLastSemesterMean,
-	getLastGoal,
-	getSpentThisMonth,
-	getHighestSpentMonth,
-	getLowestSpentMonth,
-	getLast12MonthsExpenses,
-} = require("../controllers/stats.js");
+const { getStats } = require("../controllers/stats.js");
 const router = express.Router();
 
 const { verify } = require("../middleware/verify.js");
