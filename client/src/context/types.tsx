5a24c90296f509c4c7417a3dd6d2863e30970525
hugojvb <hugojvb20@gmail.com>
2021-11-27 20:04:54 +0000

    Added set update and delete action types

--- client/src/context/types.tsx
+++ client/src/context/types.tsx
@@ -5,6 +5,15 @@ export const TOGGLE_DRAWER: string = "TOGGLE_DRAWER";
 export const GET_TRANSACTIONS: string = "GET_TRANSACTIONS";
 export const GET_GOALS: string = "GET_GOALS";
 
+export const SET_TRANSACTIONS: string = "SET_TRANSACTIONS";
+export const SET_GOALS: string = "SET_GOALS";
+
+export const DELETE_TRANSACTIONS: string = "DELETE_TRANSACTIONS";
+export const DELETE_GOALS: string = "DELETE_GOALS";
+
+export const UPDATE_TRANSACTIONS: string = "UPDATE_TRANSACTIONS";
+export const UPDATE_GOALS: string = "UPDATE_GOALS";
+
 export const SET_LOADING: string = "SET_LOADING";
 
 export const GET_STATS: string = "GET_STATS";
