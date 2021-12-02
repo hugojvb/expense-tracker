38a08931f08c418f070c87491ee5240fabeea8a7
hugojvb <hugojvb20@gmail.com>
2021-12-02 20:31:08 +0000

    Added index.html for each route

--- server.js
+++ server.js
@@ -39,13 +39,20 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
+app.get("/transactions", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+app.get("/goals", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+app.get("/summary", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 // listenning at port 5000
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () =>
-	console.log("Server running at port: ", PORT)
-);
+const server = app.listen(PORT, () => console.log("Server running at port: ", PORT));
 
 // error handling
 process.on("unhandledRejection", (err) => {
