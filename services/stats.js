f497363e3490adbfd17fe8d4552ca0729d01548a
hugojvb <hugojvb20@gmail.com>
2021-12-04 14:31:53 +0000

    Added await to getAllExpensesFromLastYear function

--- services/stats.js
+++ services/stats.js
@@ -2,6 +2,8 @@ const TransactionsSchema = require("../models/TransactionsSchema");
 const GoalsSchema = require("../models/GoalsSchema");
 const dayjs = require("dayjs");
 
+let lastYearExpenses = [];
+
 const getAllExpensesFromLastYear = async (user) => {
 	const allExpensesFromLastYear = await TransactionsSchema.find({
 		user,
@@ -10,6 +12,8 @@ const getAllExpensesFromLastYear = async (user) => {
 			$lt: dayjs(),
 		},
 	}).sort({ date: -1 });
+
+	lastYearExpenses = allExpensesFromLastYear;
 
 	return allExpensesFromLastYear;
 };
@@ -53,7 +57,7 @@ exports.getSpentThisMonthService = async (user) => {
 };
 
 exports.getHighestAndLowestSpentMonthService = async (user) => {
-	const allExpenses = getAllExpensesFromLastYear(user);
+	const allExpenses = lastYearExpenses.length === 0 ? await getAllExpensesFromLastYear(user) : lastYearExpenses;
 
 	let monthExpensesArray = [];
 	let maximumSpent = Number.NEGATIVE_INFINITY;
@@ -85,7 +89,7 @@ exports.getHighestAndLowestSpentMonthService = async (user) => {
 };
 
 exports.getLast12MonthsExpensesService = async (user) => {
-	const allExpenses = getAllExpensesFromLastYear(user);
+	const allExpenses = lastYearExpenses.length === 0 ? await getAllExpensesFromLastYear(user) : lastYearExpenses;
 
 	let monthExpensesArray = [];
 
